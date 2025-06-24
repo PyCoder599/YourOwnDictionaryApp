a5b4c7c87d1f1149cbe0bc805efe51128131fd3b
Pankaj Hurkat <aagph3348@gmail.com>
2025-06-24 15:09:10 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+import os
+import tkinter as tk
+
+
+# Window create for application
+root = tk.Tk()  # Create main Window
+root.title("YourOwn Dictionary")  # Set window title
+root.geometry("1560x780")  # Set window size (width x height)
+root.mainloop()  # Start the GUI event loop
+
